9f8a9901e340e60a370db35fe6dc24e5e93ebdf1
rubiii <me@rubiii.com>
2011-03-01 23:36:34 +0100

    inital rails stack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassonHeroku::Application.config.secret_token = '89da3ed61d79c1f88b1adb3a72271be32b285ea2c0bd4881822a4a3178ccca8a4ff69ab9e9e4b2f4c960dd4db845b661a01c6ac1af58e240762b90d5a234c83e'
